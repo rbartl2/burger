ec0c03a9e6bdbf75bde570a8972bdfa4846e33b0
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-10-31 11:04:58 -0500

    set up server and added files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+
+// Base route
+router.get('/', function(req, res){
+    res.render('index');
+})
--- server.js
+++ server.js
@@ -2,3 +2,18 @@ var express = require("express");
 var expressHandlebars = require("express-handlebars");
 
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// Sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Set up port
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:" + PORT);
+})
