33a72bc5a4ea0e727cd44a20c89d1a92b6201355
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-12-02 23:18:42 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,22 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "number41",
+    password: "password",
     database: "burgers_db"
 })
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
+
+
 
 // Create mysql connection
 connection.connect(function(err){
